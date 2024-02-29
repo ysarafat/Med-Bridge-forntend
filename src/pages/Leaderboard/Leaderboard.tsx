80b1ab6b7c3e6f08f0543360a0632cceeeecf548
Yeasir Arafat <ysarafat.bd@gmail.com>
2024-02-29 22:00:43 +0600

    fixed leaderboard

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -15,6 +15,12 @@ const Leaderboard = () => {
   return (
     <Container className="py-16">
       <SectionTitle title="leaderboard" subTitle="Our donar's ranking" />
+      <div className="mb-4  py-2 px-5  rounded-md grid grid-cols-4 justify-items-start items-center	 relative">
+        <p className="text-sm font-bold">Serial</p>
+        <p className="text-sm  col-span-2 md:col-span-1 font-semibold">Name</p>
+        <p className="col-span-2 md:col-span-1 hidden lg:block">Email</p>
+        <p className=" justify-self-end">Amount</p>
+      </div>
       {isLoading ? (
         <LeaderboardCardSkeleton />
       ) : (
